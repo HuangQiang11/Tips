2f8942f2b43f077b9d16dc7c54c06c40b94b8480
HuangQiang11 <huangqiang@huangqiangdeMac-mini.local>
2017-04-19 17:59:46 +0800

    add HQTimerTool
    add HQTimerTool

---
+++ HQTimerTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HQTimerTool
+//
+//  Created by huangqiang on 17/4/19.
+//  Copyright © 2017年 huangqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
